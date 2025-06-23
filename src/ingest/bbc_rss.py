fca9084a7920afafa67e5f9667623d221c0ee3e7
Chloe <chloever.echo@gmail.com>
2025-06-23 12:49:34 -0400

    parse raw data using feedparser

--- src/ingest/bbc_rss.py
+++ src/ingest/bbc_rss.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+import feedparser
+
+feed = feedparser.parse('bbc_world.xml')
+
+for entry in feed.entries:
+    print("Title:", entry.title)
+    print("Link:", entry.link)
+    print("Published:", entry.published)
+    print("Raw XML:\n", entry)
+    print("-" * 80)
