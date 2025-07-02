197a6aa53d81abc208a7b34f74cfff0120cbd487
Grace <grace@justinternet.com>
2025-07-02 10:37:13 -0700

    Working on testing environment on local machine

--- tests/test_rssFeeds.py
+++ tests/test_rssFeeds.py
@@ -3,7 +3,7 @@ from ingest.ap_ingestor import APIngestor
 def apLogger_task():
     # Create an instance of the class
     ingestor = APIngestor()
-    ingestor.check_and_save_new_entries(using_celery=True)  # this will invoke the inherited logic
+    ingestor.check_and_save_new_entries(using_celery=False)  # this will invoke the inherited logic
     print("AP RSS Feed checked.")
 
 
